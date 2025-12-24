2be3be2ddeaf5e26a691f9204ac0be2cd22ce13e
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-12-24 14:18:15 +0900

    refactor: 프론트 ip 수정 및 actuator health 체크 수정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://158.168.68.86:8081',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
