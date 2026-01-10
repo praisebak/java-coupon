0a55d4e4c3df0ba93217fabe8c354baf6df7b9eb
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2026-01-10 13:48:00 +0900

    fix: redis config 수정

--- src/main/kotlin/com/couponrefactroing/config/RedisConfig.kt
+++ src/main/kotlin/com/couponrefactroing/config/RedisConfig.kt
@@ -9,17 +9,21 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import io.lettuce.core.api.StatefulConnection
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration
+import org.springframework.data.redis.core.ReactiveRedisTemplate
+import org.springframework.data.redis.serializer.RedisSerializationContext
+import org.springframework.data.redis.serializer.StringRedisSerializer
 import java.time.Duration
 
 @Configuration
 class RedisConfig {
 
-    @Value("\${spring.redis.host}")
+    @Value("\${spring.data.redis.host}")
     private lateinit var host: String
 
-    @Value("\${spring.redis.port}")
+    @Value("\${spring.data.redis.port}")
     private var port: Int = 6379
 
     @Bean
@@ -49,5 +53,21 @@ class RedisConfig {
         factory.shareNativeConnection = false // [여기!] 이게 true면 풀 안씀
 
         return factory
+    }
+
+    @Bean
+    fun reactiveRedisTemplate(connectionFactory: ReactiveRedisConnectionFactory): ReactiveRedisTemplate<String, String> {
+        val serializer = StringRedisSerializer()
+
+        // 리액티브는 SerializationContext라는 걸 사용해서 직렬화를 정의합니다.
+        val serializationContext = RedisSerializationContext
+            .newSerializationContext<String, String>(serializer)
+            .key(serializer)
+            .value(serializer)
+            .hashKey(serializer)
+            .hashValue(serializer)
+            .build()
+
+        return ReactiveRedisTemplate(connectionFactory, serializationContext)
     }
 }
